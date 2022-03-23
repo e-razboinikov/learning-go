52a11a7638fc131f934f4539993798ace9b20060
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-03-23 10:28:01 +0500

    Finished lesson 3.8 Enabling Go Modules and refactoring our code to use packages.

---
+++ section_3_building_a_basic_web_application/3.8_enabling_go_modules/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/e-razboinikov/learning-go/3.8/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Application has started on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil)
+}
---
+++ section_3_building_a_basic_web_application/3.8_enabling_go_modules/go.mod
@@ -0,0 +1,3 @@
+module github.com/e-razboinikov/learning-go/3.8
+
+go 1.18
