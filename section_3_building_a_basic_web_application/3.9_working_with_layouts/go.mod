15514bdbd36565d32049bb454101a32924e23d44
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-03-25 17:22:45 +0500

    Finished lesson 3.9 Working with Layouts, and building a template cache (part one)

---
+++ section_3_building_a_basic_web_application/3.9_working_with_layouts/go.mod
@@ -0,0 +1,3 @@
+module github.com/e-razboinikov/learning-go/3.9
+
+go 1.18
---
+++ section_3_building_a_basic_web_application/3.9_working_with_layouts/pkg/handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/e-razboinikov/learning-go/3.9/pkg/render"
+)
+
+/// Home is a home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html")
+}
+
+/// About is a about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.html")
+}
